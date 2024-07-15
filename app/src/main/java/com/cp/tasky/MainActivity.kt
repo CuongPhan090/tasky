1b13895ed2941f9cede54ff55e13f5808e255709
CPeeee <cphan1363@gmail.com>
2024-07-14 23:56:34 -0700

    Initial set up navigation graph

--- app/src/main/java/com/cp/tasky/MainActivity.kt
+++ app/src/main/java/com/cp/tasky/MainActivity.kt
@@ -3,7 +3,8 @@ package com.cp.tasky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.cp.tasky.auth.login.presentation.LoginScreenRoot
+import com.cp.tasky.auth.shared.navigation.SetUpAuthGraph
+import com.cp.tasky.plannerhub.navigation.SetUpPlannerGraph
 import com.cp.tasky.ui.theme.TaskyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,7 +14,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TaskyTheme {
-                LoginScreenRoot()
+                SetUpAuthGraph()
+                SetUpPlannerGraph()
             }
         }
     }
