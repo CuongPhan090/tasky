4631e13de3a68b33a5f5e34843ca50b4a38ff6dd
CPeeee <cphan1363@gmail.com>
2024-07-16 00:43:26 -0700

    Initial set up register data layer

--- app/src/main/java/com/cp/tasky/auth/shared/data/repository/RemoteDataSource.kt
+++ app/src/main/java/com/cp/tasky/auth/shared/data/repository/RemoteDataSource.kt
@@ -3,12 +3,15 @@ package com.cp.tasky.auth.shared.data.repository
 import com.cp.tasky.auth.shared.data.remote.AuthApi
 import com.cp.tasky.auth.login.data.remote.models.LoginApiBody
 import com.cp.tasky.auth.login.data.remote.models.LoginApiResponse
+import com.cp.tasky.auth.register.data.model.UserRegisterApiBody
 import retrofit2.Response
 import javax.inject.Inject
 
 interface RemoteDataSource {
 
     suspend fun loginUser(loginApiBody: LoginApiBody): Response<LoginApiResponse>
+
+    suspend fun registerUser(registerApiBody: UserRegisterApiBody)
 }
 
 class RemoteDataSourceImpl @Inject constructor(
@@ -16,5 +19,9 @@ class RemoteDataSourceImpl @Inject constructor(
 ) : RemoteDataSource {
     override suspend fun loginUser(loginApiBody: LoginApiBody): Response<LoginApiResponse> {
         return authApi.loginUser(loginApiBody)
+    }
+
+    override suspend fun registerUser(registerApiBody: UserRegisterApiBody) {
+        authApi.registerUser(registerApiBody)
     }
 }
