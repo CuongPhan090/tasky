983facaa4fcbaf7986b9009e6ee754269713dd08
CPeeee <cphan1363@gmail.com>
2024-07-24 23:36:14 -0700

    Navigation - Migrate to Type-safe navigation and nested navGraph

--- app/src/main/java/com/cp/tasky/MainActivity.kt
+++ app/src/main/java/com/cp/tasky/MainActivity.kt
@@ -3,8 +3,11 @@ package com.cp.tasky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.cp.tasky.auth.shared.navigation.SetUpAuthGraph
-import com.cp.tasky.plannerhub.navigation.SetUpPlannerGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.cp.tasky.auth.shared.navigation.setUpAuthGraph
+import com.cp.tasky.agenda.navigation.AgendaScreen
+import com.cp.tasky.agenda.navigation.setUpPlannerGraph
 import com.cp.tasky.ui.theme.TaskyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,8 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TaskyTheme {
-                SetUpAuthGraph()
-                SetUpPlannerGraph()
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = AgendaScreen.Overview) {
+                    setUpAuthGraph(navController = navController)
+                    setUpPlannerGraph(navController = navController)
+                }
             }
         }
     }
--- app/src/main/java/com/cp/tasky/plannerhub/navigation/PlannerScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.cp.tasky.plannerhub.navigation
-
-sealed class PlannerScreen(val route: String) {
-    data object Agenda: PlannerScreen("agenda")
-    data object TaskDetail: PlannerScreen("task_detail") // prob need an id
-    data object EventDetail: PlannerScreen("event_detail") // prob need an id
-    data object ReminderDetail: PlannerScreen("reminder_detail") // prob need an id
-}
